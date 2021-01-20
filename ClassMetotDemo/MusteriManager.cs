6bd9d0e25b928d4e271f700f1cd7de70eed24734
AyseOten <ayseguler.92@gmail.com>
2021-01-20 21:13:08 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine( musteri.Name + " " + musteri.Surname + " Eklendi");
+        }
+        public void MusteriSil(int AccountNum)
+        {
+            Console.WriteLine(AccountNum + " " + "no lu hesap silindi.");
+        }
+        public void MusteriListele(Musteri[] musteris)
+        {
+            foreach (Musteri musteri in musteris)
+            {
+                Console.WriteLine("---------Musteriler-------------");
+                Console.WriteLine(musteri.Name);
+                Console.WriteLine(musteri.Surname);
+                Console.WriteLine(musteri.AccountNumber);
+                Console.WriteLine("----------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Ayse";
+            musteri1.Surname = "Sevde";
+            musteri1.AccountNumber = 2821;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Hüseyin";
+            musteri2.Surname = "Fatih";
+            musteri2.AccountNumber = 2930;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Name = "Omer";
+            musteri3.Surname = "Tarik";
+            musteri3.AccountNumber = 1602;
+
+            Musteri[] Musteriler = new Musteri[] { musteri1};
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.MusteriEkle(musteri2);
+            musterimanager.MusteriEkle(musteri3);
+
+            musterimanager.MusteriSil(1602);
+            musterimanager.MusteriListele(Musteriler);
+
+        }
+    }
+}
